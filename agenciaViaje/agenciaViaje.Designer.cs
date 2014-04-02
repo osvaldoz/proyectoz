1270a93f6c8774b115eb18aa6482481d1eb18b30
osvaldo <OSVALDOZ-@hotmail.com>
2014-04-02 08:13:31 -0700

    Versionas 2

---
+++ agenciaViaje/agenciaViaje.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace agenciaViaje
+{
+    partial class agenciaViaje
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Origen = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // Origen
+            // 
+            this.Origen.AutoSize = true;
+            this.Origen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Origen.Location = new System.Drawing.Point(26, 33);
+            this.Origen.Name = "Origen";
+            this.Origen.Size = new System.Drawing.Size(51, 20);
+            this.Origen.TabIndex = 0;
+            this.Origen.Text = "label1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(120, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // agenciaViaje
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 299);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Origen);
+            this.Name = "agenciaViaje";
+            this.Text = "agenciaViaje";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Origen;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
